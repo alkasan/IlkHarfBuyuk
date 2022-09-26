f72bf1f1854503bd740f0896dd023aeebf970f4a
ealka <ealkasan@hotmail.com>
2022-09-26 20:40:59 +0300

    Add project files.

---
+++ GirilenCümleninİlkHarfiBuyuk/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IlkHarfBuyuk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cümle Giriniz: ");
+            string cumle = Console.ReadLine();
+            string[] dizi = cumle.Split(' ');
+            for (int i = 0; i < dizi.Length; i++)
+            dizi[i] = dizi[i][0].ToString().ToUpper() + dizi[i].Substring(1);
+            string ilkHarfBuyuk = string.Join(" ", dizi);
+            Console.WriteLine(ilkHarfBuyuk);
+        }
+    }
+}
+
